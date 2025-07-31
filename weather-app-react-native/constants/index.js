4e7e8ea283f42dd6cb16568bfdc795ead3094fbf
ahalyajupalli <35890348+ahalyajupalli@users.noreply.github.com>
2025-07-31 01:37:01 -0600

    Update index.js

--- weather-app-react-native/constants/index.js
+++ weather-app-react-native/constants/index.js
@@ -1,4 +1,4 @@
-//export const apiKey = 'place your api key here from : https://www.weatherapi.com';
+
 
 export const weatherImages = {
     'Partly cloudy': require('../assets/images/partlycloudy.png'),
